c3c7c7b95a32d6ee2aec6e64e8fb650ee0f8bd48
Govnor Payne <govnorpayne@gmail.com>
2022-08-18 21:19:19 -0500

    added frontend

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,16 +3,49 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    Todos: [ToDo]
+    friends: [User]
+  }
+
   type ToDo {
     _id: ID
     ToDoText: String
     createdAt: String
     username: String
     reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
     ToDos(username: String): [ToDo]
+    ToDo(_id: ID!): ToDo
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addToDo(ToDoText: String!): ToDo
+    addReaction(ToDoId: ID!, reactionBody: String!): ToDo
+    addFriend(friendId: ID!): User
   }
 `;
 
