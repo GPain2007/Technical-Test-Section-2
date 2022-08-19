23a3598492777235b83b5386647a60d92dced642
Govnor Payne <govnorpayne@gmail.com>
2022-08-18 22:51:38 -0500

    added resolvers.js back

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,114 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, ToDo } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("ToDos")
+          .populate("friends");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("ToDos")
+        .populate("friends");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("ToDos");
+    },
+    ToDos: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return ToDo.find(params).sort({ createdAt: -1 });
+    },
+    ToDo: async (parent, { _id }) => {
+      return ToDo.findOne({ _id });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addToDo: async (parent, args, context) => {
+      if (context.user) {
+        const Todo = await ToDo.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { ToDos: Todo._id } },
+          { new: true }
+        );
+
+        return Todo;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    addReaction: async (parent, { ToDoId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedToDo = await ToDo.findOneAndUpdate(
+          { _id: ToDoId },
+          {
+            $push: {
+              reactions: { reactionBody, username: context.user.username },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return updatedToDo;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate("friends");
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
+module.exports = resolvers;
